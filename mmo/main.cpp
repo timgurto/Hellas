8ee84357e895dd5c2d82de72fce7f04fce16a792
Tim Gurto <timgurto@gmail.com>
2015-05-23 11:34:46 +1000

    Move server's socket handling to new thread

--- mmo/main.cpp
+++ mmo/main.cpp
@@ -1,12 +1,21 @@
 #include <string>
+#include <windows.h>
+
 #include "Socket.h"
+
+DWORD WINAPI f(LPVOID socket){
+    ((Socket*)socket)->runServer();
+    return 0;
+}
 
 int main(int argc, char** argv){
     Socket socket;
     if (argc > 1 && std::string(argv[1]) == "-server") {
-        socket.runServer();
+        DWORD socketThreadID;
+        HANDLE socketThreadHandle = CreateThread(0, 0, &f, &socket, 0, &socketThreadID);
     } else {
         socket.runClient();
         socket.sendCommand("Test");
     }
+    std::cout << "Execution is here" << std::endl;
 }
--- mmo/socket.h
+++ mmo/socket.h
@@ -3,7 +3,7 @@
 
 #include <iostream>
 #include <string>
-#include <winsock2.h>
+#include <windows.h>
 
 #pragma comment(lib, "ws2_32.lib")
 
