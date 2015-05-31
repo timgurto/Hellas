53ff7b0066b1171d3e50e0d2dfaf57662f73d368
Tim Gurto <timgurto@gmail.com>
2015-05-31 16:35:39 +1000

    Batch file stuff

--- test.bat
+++ test.bat
@@ -1,9 +1,11 @@
+rem xcopy /y /d Debug\mmo.exe .
+rem for /L %%x in (805, 645, 3195) do for /L %%y in (25, 510, 1650) do (
+rem     echo %%x %%y
+rem     start mmo.exe -left %%x -top %%y -server-ip 192.168.1.5
+rem     rem timeout 1
+rem )
+
 TASKKILL /F /IM "mmo.exe"
 xcopy /y /d Debug\mmo.exe .
-start mmo.exe -server -left 10 -top 25
-timeout 1
-start mmo.exe -left 815 -top 25 -server-ip 127.0.0.1
-timeout 1
-start mmo.exe -left 815 -top 535 -server-ip 192.168.1.5
-timeout 1
-start mmo.exe -left 1460 -top 25 -server-ip 121.44.199.135 
+start mmo.exe -server-ip 192.168.1.5
+mmo.exe -left 5 -top 25 -server
