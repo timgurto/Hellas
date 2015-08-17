6c4e9b299c8ba0266688c0d723a21ca72929d421
Tim Gurto <timgurto@gmail.com>
2015-08-17 23:54:52 +1000

    Add reactive scrollbar to List

--- mmo/List.cpp
+++ mmo/List.cpp
@@ -1,20 +1,66 @@
 // (C) 2015 Tim Gurto
 
 #include "Label.h"
+#include "Line.h"
 #include "List.h"
 #include "Renderer.h"
+#include "ShadowBox.h"
 
 extern Renderer renderer;
+
+const int List::ARROW_W = 8;
+const int List::ARROW_H = 5;
+const int List::CURSOR_HEIGHT = 5;
 
 List::List(const SDL_Rect &rect, int childHeight):
 Element(rect),
 _childHeight(childHeight),
-_content(new Element(makeRect(0, 0, rect.w - SCROLL_BAR_WIDTH, 0))){
+_content(new Element(makeRect(0, 0, rect.w - ARROW_W, 0))),
+_scrollBar(new Element(makeRect(rect.w - ARROW_W, 0, ARROW_W, rect.h))),
+_cursor(new Element(makeRect(0, 0, ARROW_W, CURSOR_HEIGHT))),
+_scrollY(0){
     Element::addChild(_content);
-    addChild(new Label(makeRect(), "label"));
+    Element::addChild(_scrollBar);
+
+    // Scroll bar details
+    _scrollBar->addChild(new Line(ARROW_W/2 - 1, ARROW_H, rect.h - 2*ARROW_H, VERTICAL));
+
+    _whiteUp = new Picture(makeRect(0, 0, ARROW_W, ARROW_H),
+                           Texture("Images/arrowUp.png", Color::MAGENTA));
+    _greyUp = new Picture(makeRect(0, 0, ARROW_W, ARROW_H),
+                          Texture("Images/arrowUpGrey.png", Color::MAGENTA));
+    _whiteDown = new Picture(makeRect(0, rect.h - ARROW_H, ARROW_W, ARROW_H),
+                             Texture("Images/arrowDown.png", Color::MAGENTA)),
+    _greyDown = new Picture(makeRect(0, rect.h - ARROW_H, ARROW_W, ARROW_H),
+                            Texture("Images/arrowDownGrey.png", Color::MAGENTA));
+    _whiteUp->hide();
+    _greyDown->hide();
+    _scrollBar->addChild(_whiteUp);
+    _scrollBar->addChild(_greyUp);
+    _scrollBar->addChild(_whiteDown);
+    _scrollBar->addChild(_greyDown);
+
+    _cursor->fillBackground();
+    _cursor->addChild(new ShadowBox(_cursor->rect()));
+    _scrollBar->addChild(_cursor);
+    updateCursor();
+}
+
+void List::updateCursor(){
+    static const int Y_MIN = ARROW_H - 1;
+    const int Y_MAX = _scrollBar->rect().h - ARROW_H + 1;
+    const int Y_RANGE = Y_MAX - Y_MIN;
+    double progress = -1.0 * _content->rect().y / (_content->rect().h - rect().h);
+    _cursor->rect(0, static_cast<int>(progress * Y_RANGE + Y_MIN + .5));
 }
 
 void List::refresh(){
+    // Hide scroll bar if not needed
+    if (_content->rect().h <= rect().h)
+        _scrollBar->hide();
+    else
+        _scrollBar->show();
+
     renderer.pushRenderTarget(_texture);
 
     drawChildren();
@@ -25,7 +71,7 @@ void List::refresh(){
 void List::addChild(Element *child){
     child->rect(0,
                 _content->height(),
-                rect().w - SCROLL_BAR_WIDTH,
+                rect().w - ARROW_W,
                 _childHeight);
     _content->height(_content->height() + _childHeight);
     _content->addChild(child);
--- mmo/List.h
+++ mmo/List.h
@@ -4,14 +4,28 @@
 #define LIST_H
 
 #include "Element.h"
+#include "Picture.h"
 
 // A scrollable vertical list of elements of uniform height
 class List : public Element{
-    static const int SCROLL_BAR_WIDTH = 10;
+    static const int
+        ARROW_W,
+        ARROW_H,
+        CURSOR_HEIGHT;
+
+    int _scrollY; // Offset, in pixels; should always be 0 or negative.
 
     int _childHeight;
 
     Element *_content; // Holds the list items themselves, and moves up and down to "scroll".
+    Element *_scrollBar;
+    Picture // Arrow images
+        *_whiteUp,
+        *_whiteDown,
+        *_greyUp,
+        *_greyDown;
+    Element *_cursor; // Shows position on the scroll bar, and can be dragged.
+    void updateCursor(); // Set the cursor location based on _scrollY.
 
     virtual void refresh();
 
