b4dfbd3eaecb2d4c77b58f1bc08c067e143a616c
Tim Gurto <timgurto@gmail.com>
2015-10-05 13:46:11 +1100

    Fix inventory-window location and dimensions

--- mmo/Container.cpp
+++ mmo/Container.cpp
@@ -3,7 +3,7 @@
 #include "Container.h"
 
 Container::Container(size_t rows, size_t cols, containerVec_t &linked, int x, int y):
-Element(makeRect(x, y, rows * Client::ICON_SIZE, cols * Client::ICON_SIZE)),
+Element(makeRect(x, y, cols * Client::ICON_SIZE, rows * Client::ICON_SIZE)),
 _rows(rows),
 _cols(cols),
 _linked(linked){}
--- mmo/inventoryWindow.cpp
+++ mmo/inventoryWindow.cpp
@@ -2,18 +2,19 @@
 
 #include "Client.h"
 #include "Container.h"
+#include "User.h"
 #include "Window.h"
 
 void Client::initializeInventoryWindow(){
     static const int
-        ROWS = 2,
-        COLS = 3;
+        COLS = 4,
+        ROWS = (User::INVENTORY_SIZE - 1) / COLS + 1;
     Container *inventory = new Container(ROWS, COLS, _inventory);
     const int
-        LEFT = 50,
-        TOP = 300;
         HEIGHT = inventory->height(),
         WIDTH = inventory->width(),
+        LEFT = 640 - WIDTH - 6,
+        TOP = 360 - HEIGHT - 18;
 
     _inventoryWindow = new Window(makeRect(LEFT, TOP, WIDTH, HEIGHT), "Inventory");
     _inventoryWindow->addChild(inventory);
