49a989e3021712f5e81e686922e31037628bd0e4
Tim Gurto <timgurto@gmail.com>
2015-11-22 07:57:35 +1100

    Allow labels to have custom colors

--- src/client/ui/Label.h
+++ src/client/ui/Label.h
@@ -7,11 +7,14 @@
 
 #include "Element.h"
 
+class Color;
+
 // Displays text.
 class Label : public Element{
     std::string _text;
     Justification _justificationH, _justificationV;
     bool _matchWidth;
+    Color _color;
 
 public:
     Label(const Rect &rect, const std::string &text,
@@ -19,6 +22,8 @@ public:
           Justification justificationV = TOP_JUSTIFIED);
 
     virtual void refresh() override;
+
+    void setColor(const Color &color);
 
     void matchW(); // Set the label's width to the width of the contained text image.
 };
