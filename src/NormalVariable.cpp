016948aad1a38b16ac85e0ab741610cda3a65dea
Tim Gurto <timgurto@gmail.com>
2016-09-21 00:06:42 -0700

    Follow rule of three for NormalVariable

--- src/NormalVariable.cpp
+++ src/NormalVariable.cpp
@@ -7,6 +7,7 @@ std::default_random_engine NormalVariable::randomEngine;
 NormalVariable::NormalVariable(double mean, double sd):
 _zeroSD(false),
 _mean(mean),
+_sd(sd),
 _dist(nullptr){
     if (sd == 0)
         _zeroSD = true;
@@ -14,9 +15,34 @@ _dist(nullptr){
         _dist = new std::normal_distribution<double>(mean, sd);
 }
 
+NormalVariable::NormalVariable(const NormalVariable &rhs):
+_zeroSD(rhs._zeroSD),
+_mean(rhs._mean),
+_sd(rhs._sd),
+_dist(nullptr)
+{
+    if (!_zeroSD)
+        _dist = new std::normal_distribution<double>(_mean, _sd);
+}
+
 NormalVariable::~NormalVariable(){
     if (_dist != nullptr)
         delete _dist;
+}
+
+NormalVariable &NormalVariable::operator=(const NormalVariable &rhs){
+    if (this != &rhs){
+        if (_dist != nullptr)
+            delete _dist;
+        _zeroSD = rhs._zeroSD;
+        _mean = rhs._mean;
+        _sd = rhs._sd;
+        if (!_zeroSD)
+            _dist = new std::normal_distribution<double>(_mean, _sd);
+        else
+            _dist = nullptr;
+    }
+    return *this;
 }
 
 double NormalVariable::generate() const{
--- src/NormalVariable.h
+++ src/NormalVariable.h
@@ -8,14 +8,17 @@
 // Generates random numbers with a normal distribution, based on provided mean/SD
 class NormalVariable{
     bool _zeroSD;
-    double _mean;
+    double _mean, _sd;
     std::normal_distribution<double> *_dist;
 
     static std::default_random_engine randomEngine;
 
 public:
     NormalVariable(double mean = 0, double sd = 1);
+    NormalVariable(const NormalVariable &rhs);
     ~NormalVariable();
+    NormalVariable &operator=(const NormalVariable &rhs);
+
     double generate() const;
     double operator()() const { return generate(); }
 };
