52195ba5e97384ba8654836e388f09fd9001b512
Tim Gurto <timgurto@gmail.com>
2016-09-26 04:06:54 -0700

    Add test to run server

--- src/testing/test-objects.cpp
+++ src/testing/test-objects.cpp
@@ -2,13 +2,25 @@
 
 #include <cstdlib>
 #include <SDL.h>
+#include <thread>
 
 #include "Test.h"
 #include "../client/Client.h"
 #include "../server/Server.h"
 
-TEST("Run client and server")
+TEST("Create server and client objects")
     Server s;
     Client c;
+    return true;
+TEND
+
+template<typename T>
+static void start(T *t){
+    t->run();
+}
+
+TEST("Start server")
+    Server server;
+    std::thread(start<Server>, &server).detach();
     return true;
 TEND
