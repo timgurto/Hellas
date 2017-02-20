977a5f3ad8602b563ef014ed95d96da740917a78
Tim Gurto <timgurto@gmail.com>
2017-02-21 05:46:58 +1100

    Include items, recipes, NPCs in tech-tree generator

--- doc/TreeGen/TreeGen/generate.cpp
+++ doc/TreeGen/TreeGen/generate.cpp
@@ -19,6 +19,24 @@ int main(){
     }
 
     // Load items
+    if (!xr.newFile(dataPath + "/items.xml"))
+        std::cerr << "Failed to load items.xml" << std::endl;
+    else{
+        for (auto elem : xr.getChildren("item")) {
+            std::string id;
+            if (!xr.findAttr(elem, "id", id))
+                continue;
+            std::string label = "item_" + id;
+
+            std::string s;
+            if (xr.findAttr(elem, "name", s))
+                nodes.insert(std::make_pair(label, s));
+
+            if (xr.findAttr(elem, "constructs", s)){
+                    map.insert(std::make_pair(label, "object_" + s));
+            }
+        }
+    }
 
     // Load objects
     if (!xr.newFile(dataPath + "/objectTypes.xml"))
@@ -54,9 +72,55 @@ int main(){
     }
 
     // Load NPCs
+    if (!xr.newFile(dataPath + "/npcTypes.xml"))
+        std::cerr << "Failed to load npcTypes.xml" << std::endl;
+    else{
+        for (auto elem : xr.getChildren("npcType")) {
+            std::string id;
+            if (!xr.findAttr(elem, "id", id))
+                continue;
+            std::string label = "npc_" + id;
+
+            std::string s;
+            if (xr.findAttr(elem, "name", s))
+                nodes.insert(std::make_pair(label, s));
+
+            for (auto loot : xr.getChildren("loot", elem)) {
+                if (!xr.findAttr(loot, "id", s))
+                    continue;
+                map.insert(std::make_pair(label, "item_" + s));
+            }
+        }
+    }
 
 
     // Load recipes
+    if (!xr.newFile(dataPath + "/recipes.xml"))
+        std::cerr << "Failed to load recipes.xml" << std::endl;
+    else{
+        for (auto elem : xr.getChildren("recipe")) {
+            std::string product;
+            if (!xr.findAttr(elem, "product", product))
+                continue;
+            product = "item_" + product;
+
+            std::string s;
+            for (auto material : xr.getChildren("material", elem))
+                if (xr.findAttr(material, "id", s))
+                    map.insert(std::make_pair("item_" + s, product));
+
+            for (auto material : xr.getChildren("tool", elem)){
+                if (!xr.findAttr(material, "class", s))
+                    continue;
+                auto it = tools.find(s);
+                if (it == tools.end()){
+                    std::cerr << "Tool class is missing archetype: " << s << std::endl;
+                    map.insert(std::make_pair(s, product));
+                } else
+                    map.insert(std::make_pair(it->second, product));
+        }
+        }
+    }
 
 
     // Publiah
