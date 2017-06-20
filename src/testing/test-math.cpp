badce966be80b613150667b69518d70d5df2b845
Tim Gurto <timgurto@gmail.com>
2017-06-21 05:32:27 +1000

    Move math tests to Catch

--- src/testing/test-math.cpp
+++ src/testing/test-math.cpp
@@ -8,26 +8,28 @@
 #include "../NormalVariable.h"
 #include "../server/Server.h"
 
-TEST("Convert from Uint32 to Color and back")
+#include "catch.hpp"
+
+TEST_CASE("Convert from Uint32 to Color and back"){
     Uint32 testNum = rand();
     Color c = testNum;
     Uint32 result = c;
-    return testNum == result;
-TEND
+    CHECK(testNum == result);
+}
 
-TEST("Zero-SD normal variables")
-    NormalVariable
-        a(0, 0),
-        b(1, 0),
-        c(-0.5, 0);
-    return
-        a.generate() == 0 &&
-        a() == 0 &&
-        b.generate() == 1 &&
-        c.generate() == -0.5;
-TEND
+TEST_CASE("Zero-SD normal variables"){
+    NormalVariable a(0, 0);
+    CHECK( a.generate() == 0);
+    CHECK(a() == 0);
 
-TEST("Complex normal variable")
+    NormalVariable b(1, 0);
+    CHECK(b.generate() == 1);
+
+    NormalVariable c(-0.5, 0);
+    CHECK(c.generate() == -0.5);
+}
+
+TEST_CASE("Complex normal variable"){
     NormalVariable default;
     NormalVariable v(10, 2);
     static const size_t SAMPLES = 1000;
@@ -45,15 +47,14 @@ TEST("Complex normal variable")
         proportionWithin1SD = 1.0 * numWithin1SD / SAMPLES,
         proportionBetween1and2SD = 1.0 * (numWithin2SD - numWithin1SD) / SAMPLES;
     // Proportion within 1 standard deviation should be around 0.68
-    if (proportionWithin1SD < 0.63 || proportionWithin1SD > 0.73)
-        return false;
+    CHECK (proportionWithin1SD >= 0.63);
+    CHECK (proportionWithin1SD <= 0.73);
     // Proportion between 1 and 2 standard deviations should be around 0.27
-    if (proportionBetween1and2SD < 0.22 || proportionBetween1and2SD > 0.32)
-        return false;
-    return true;
-TEND
+    CHECK (proportionBetween1and2SD >= 0.22);
+    CHECK (proportionBetween1and2SD <= 0.32);
+}
 
-TEST("NormalVariable copying")
+TEST_CASE("NormalVariable copying"){
     bool ret = true;
     
     auto prevHandler = std::signal(SIGSEGV, Test::signalThrower); // Set signal handler
@@ -70,25 +71,26 @@ TEST("NormalVariable copying")
     _CrtSetReportMode(_CRT_ASSERT,prevAssertMode); // Re-enable asserts
     std::signal(SIGSEGV, prevHandler); // Reset signal handler
 
-    return ret;
-TEND
+    CHECK(ret);
+}
 
-TEST("Distance-to-line with A=B")
+TEST_CASE("Distance-to-line with A=B"){
     Point p(10, 10);
     Point q(5, 3);
     distance(p, q, q);
-    return true;
-TEND
+}
 
-TEST("Tile rect 0,0") return (Server::getTileRect(0, 0) == Rect(-16, 0, 32, 32)); TEND
-TEST("Tile rect 1,0") return (Server::getTileRect(1, 0) == Rect(16, 0, 32, 32));  TEND
-TEST("Tile rect 0,1") return (Server::getTileRect(0, 1) == Rect(0, 32, 32, 32));  TEND
-TEST("Tile rect 1,1") return (Server::getTileRect(1, 1) == Rect(32, 32, 32, 32)); TEND
+TEST_CASE("getTileRect() behaves as expected"){
+CHECK(Server::getTileRect(0, 0) == Rect(-16, 0, 32, 32));
+CHECK(Server::getTileRect(1, 0) == Rect(16, 0, 32, 32));
+CHECK(Server::getTileRect(0, 1) == Rect(0, 32, 32, 32));
+CHECK(Server::getTileRect(1, 1) == Rect(32, 32, 32, 32));
 
-TEST("Tile rect 5,0") return (Server::getTileRect(4, 0) == Rect(112, 0, 32, 32));   TEND
-TEST("Tile rect 5,0") return (Server::getTileRect(5, 0) == Rect(144, 0, 32, 32));   TEND
-TEST("Tile rect 0,5") return (Server::getTileRect(0, 4) == Rect(-16, 128, 32, 32)); TEND
-TEST("Tile rect 0,5") return (Server::getTileRect(0, 5) == Rect(0, 160, 32, 32));   TEND
+CHECK(Server::getTileRect(4, 0) == Rect(112, 0, 32, 32));
+CHECK(Server::getTileRect(5, 0) == Rect(144, 0, 32, 32));
+CHECK(Server::getTileRect(0, 4) == Rect(-16, 128, 32, 32));
+CHECK(Server::getTileRect(0, 5) == Rect(0, 160, 32, 32));
 
-TEST("Tile rect odd Y")  return (Server::getTileRect(7, 5) == Rect(224, 160, 32, 32)); TEND
-TEST("Tile rect even Y") return (Server::getTileRect(7, 6) == Rect(208, 192, 32, 32)); TEND
+CHECK(Server::getTileRect(7, 5) == Rect(224, 160, 32, 32));
+CHECK(Server::getTileRect(7, 6) == Rect(208, 192, 32, 32));
+}
