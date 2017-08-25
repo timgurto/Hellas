ca4e8fb878225abb4b4f655c468acd6fd0a1a10e
Tim Gurto <timgurto@gmail.com>
2017-08-26 05:51:32 +1000

    Refactor wife test slightly

--- src/testing/test-altars.cpp
+++ src/testing/test-altars.cpp
@@ -3,22 +3,23 @@
 #include "testing.h"
 
 TEST_CASE("A user can't build multiple player-unique objects", "[player-unique]") {
-    // Given a server and client;
-    // And "blonde" and "readhead" object types, each buildable with an engagement ring;
+    // Given "blonde" and "readhead" object types, each buildable with an engagement ring;
     // And each is marked with the "wife" player-unique category;
     auto s = TestServer::WithData("wives");
-    auto c = TestClient::WithData("wives");
 
-    // And the user has an engagement ring;
+    // And a user named Bob
+    auto c = TestClient::WithUsernameAndData("bob", "wives");
+
+    // And Bob has a blonde wife
+    s.addObject("blonde", {}, "bob");
+
+    // And Bob has an engagement ring;
     WAIT_UNTIL(s.users().size() == 1);
     auto &user = s.getFirstUser();
     auto &engagementRing = s.getFirstItem();
     user.giveItem(&engagementRing);
 
-    // And the user already has a wife
-    s.addObject("blonde", {}, user.name());
-
-    // When the user tries to get another wife
+    // When Bob tries to get a readhead wife
     c.sendMessage(CL_CONSTRUCT, makeArgs("redhead", 10, 10));
 
     // Then there is still only one in the world
