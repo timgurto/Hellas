6d1327098ba72d73f7c55784f2168ec666f88743
Tim Gurto <timgurto@gmail.com>
2017-09-02 09:09:50 +1000

    Draw page title in help window

--- src/client/HelpEntry.cpp
+++ src/client/HelpEntry.cpp
@@ -12,15 +12,26 @@ void HelpEntry::addParagraph(size_t order, const std::string &heading, const std
 
 void HelpEntry::draw(List * page) const {
     page->clearChildren();
+    auto *title = new Label({}, "-- " + _name + " --",  Element::CENTER_JUSTIFIED);
+    title->setColor(Color::HELP_TEXT_HEADING);
+    page->addChild(title);
+    page->addChild(new Element);
+
+    bool pageIsEmpty = true;
+
     for (const auto &paragraph : _paragraphs) {
+        // Draw heading
         if (!paragraph.heading.empty()) {
-            if (_paragraphs.size() > 1)
+            if (!pageIsEmpty)
                 page->addChild(new Element);
             auto *heading = new Label({}, paragraph.heading);
             heading->setColor(Color::HELP_TEXT_HEADING);
             page->addChild(heading);
         }
+
+        // Draw paragraph
         page->addChild(new Label({}, paragraph.text));
+        pageIsEmpty = false;
     }
 }
 
