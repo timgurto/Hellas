71cb292591be0e601e9f9a79c79b75d7e9178423
Tim Gurto <timgurto@gmail.com>
2017-10-31 03:13:05 +1100

    Allow missing spell icons

--- src/client/ClientSpell.cpp
+++ src/client/ClientSpell.cpp
@@ -8,8 +8,6 @@
 ClientSpell::ClientSpell(const std::string &id) :
     _castMessage(Client::compileMessage(CL_CAST, id)),
     _icon("Images/Spells/"s + id + ".png"s){
-    
-    assert(_icon);
 }
 
 const Texture &ClientSpell::tooltip() const {
