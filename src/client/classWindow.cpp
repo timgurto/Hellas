28ab1d41bc051b611324edd8d16be46c87344b0c
Tim Gurto <timgurto@gmail.com>
2017-11-20 04:09:08 +1100

    Add spell icons to class window

--- src/client/classWindow.cpp
+++ src/client/classWindow.cpp
@@ -14,6 +14,7 @@ void Client::populateClassWindow() {
         auto learnSpellButton = new Button({ 0, y, 18, 18 }, ""s,
             this->sendRawMessageStatic, learnMessageVoidPtr);
         learnSpellButton->setTooltip(spell->name());
+        learnSpellButton->addChild(new Picture(1, 1, spell->icon()));
 
         _classWindow->addChild(learnSpellButton);
         y += 18;
