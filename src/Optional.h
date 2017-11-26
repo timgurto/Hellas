1304f1d7ebac7f321ac4443b556f94c550269291
Tim Gurto <timgurto@gmail.com>
2017-11-27 02:32:30 +1100

    Add Optional class in lieu of std::optional in VS

---
+++ src/Optional.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <memory>
+
+template<typename T>
+class Optional {
+public:
+    Optional() :
+        _p{ nullptr } {
+    }
+
+    Optional(const Optional &rhs) {
+        if (rhs.hasValue())
+            _p = new T(rhs.value());
+    }
+
+    ~Optional() {
+        if (_p)
+            delete _p;
+    }
+
+    const T &operator=(const T &rhs) {
+        if (hasValue())
+            delete _p;
+        _p = new T(rhs);
+
+        return *_p;
+    }
+
+    bool hasValue() const {
+        return _p != nullptr;
+    }
+
+    const T &value() const {
+        assert(_p);
+        return *_p;
+    }
+
+    T &value() {
+        assert(_p);
+        return *_p;
+    }
+
+private:
+    T *_p{ nullptr };
+};
