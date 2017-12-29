67fe258ce10690a0ccddecaa9370b16abf20f2d8
Tim Gurto <timgurto@gmail.com>
2017-12-29 18:08:38 +1100

    Reduce creation/destruction of dummy Users

--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -425,9 +425,12 @@ void Server::removeUser(const Socket &socket){
 }
 
 std::set<User *> Server::findUsersInArea(MapPoint loc, double squareRadius) const{
+    static User dummy{ MapPoint{0} };
     std::set<User *> users;
-    auto loX = _usersByX.lower_bound(&User({ loc.x - squareRadius, 0 }));
-    auto hiX = _usersByX.upper_bound(&User({ loc.x + squareRadius, 0 }));
+    dummy.location(loc.x - squareRadius);
+    auto loX = _usersByX.lower_bound(&dummy);
+    dummy.location(loc.x + squareRadius);
+    auto hiX = _usersByX.upper_bound(&dummy);
     for (auto it = loX; it != hiX; ++it)
         if (abs(loc.y - (*it)->location().y) <= squareRadius)
             users.insert(const_cast<User *>(*it));
