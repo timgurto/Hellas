36289e9db98d29e0da3c1861654df640598b0fd5
Tim Gurto <timgurto@gmail.com>
2018-04-06 03:00:36 +1000

    Remove unnecessary movement of new users in tests
    Users should already be at the desired location, due to the spawn point
    prescribed in minimal/map.xml.

--- src/testing/test-gathering.cpp
+++ src/testing/test-gathering.cpp
@@ -3,26 +3,23 @@
 #include "testing.h"
 
 TEST_CASE("Gather an item from an object"){
-    TestServer s = TestServer::WithData("basic_rock");
-    TestClient c = TestClient::WithData("basic_rock");
-
-    //Move user to middle
-    s.waitForUsers(1);
-    User &user = s.getFirstUser();
-    user.updateLocation({ 10, 10 });
+    auto s = TestServer::WithData("basic_rock");
+    auto c = TestClient::WithData("basic_rock");
 
     // Add a single rock
     s.addObject("rock", { 10, 10 });
+    s.waitForUsers(1);
     WAIT_UNTIL (c.objects().size() == 1);
 
     //Gather
-    size_t serial = c.objects().begin()->first;
+    auto serial = c.objects().begin()->first;
     c.sendMessage(CL_GATHER, makeArgs(serial));
+    auto &user = s.getFirstUser();
     WAIT_UNTIL (user.action() == User::Action::GATHER) ; // Wait for gathering to start
     WAIT_UNTIL (user.action() == User::Action::NO_ACTION) ; // Wait for gathering to finish
 
     //Make sure user has item
-    const Item &item = *s.items().begin();
+    const auto &item = *s.items().begin();
     CHECK(user.inventory()[0].first == &item);
 
     //Make sure object no longer exists
@@ -33,33 +30,31 @@ TEST_CASE("Gather an item from an object"){
 One gather worth of 1 million units of iron
 1000 gathers worth of single rocks
 This is to test the new gather algorithm, which would favor rocks rather than iron.
+It will fail randomly about 1 every 1000 times
 */
 TEST_CASE("Gather chance is by gathers, not quantity"){
-    TestServer s = TestServer::WithData("rare_iron");
-    TestClient c = TestClient::WithData("rare_iron");
-
-    //Move user to middle
-    WAIT_UNTIL (s.users().size() == 1);
-    User &user = s.getFirstUser();
-    user.updateLocation({ 10, 10 });
+    auto s = TestServer::WithData("rare_iron");
+    auto c = TestClient::WithData("rare_iron");
 
     // Add a single iron deposit
     s.addObject("ironDeposit", { 10, 10 });
+    s.waitForUsers(1);
     WAIT_UNTIL (c.objects().size() == 1);
 
     //Gather
-    size_t serial = c.objects().begin()->first;
+    auto serial = c.objects().begin()->first;
     c.sendMessage(CL_GATHER, makeArgs(serial));
+    auto &user = s.getFirstUser();
     WAIT_UNTIL (user.action() == User::Action::GATHER) ; // Wait for gathering to start
     WAIT_UNTIL (user.action() == User::Action::NO_ACTION) ; // Wait for gathering to finish
 
     //Make sure user has a rock, and not the iron
-    const ServerItem &item = *s.items().find(ServerItem("rock"));
+    const auto &item = *s.items().find(ServerItem("rock"));
     CHECK(user.inventory()[0].first == &item);
 }
 
 TEST_CASE("Minimum yields"){
-    TestServer s = TestServer::WithData("min_apples");
+    auto s = TestServer::WithData("min_apples");
     for (auto entity : s.entities()){
         const Object *obj = dynamic_cast<const Object *>(entity);
         CHECK_FALSE(obj->contents().isEmpty());
