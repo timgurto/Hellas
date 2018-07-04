9ef66ef29e463f68c4b11b8156e0cbcea1333b9e
Tim Gurto <timgurto@gmail.com>
2018-07-04 12:44:56 +1000

    Add quest name to window

--- src/client/CQuest.cpp
+++ src/client/CQuest.cpp
@@ -4,8 +4,22 @@
 #include "ui/Window.h"
 
 void CQuest::generateWindow(void *questAsVoid) {
+  const auto WIN_W = 200_px, WIN_H = 200_px;
+
   auto quest = reinterpret_cast<CQuest *>(questAsVoid);
-  quest->_window = Window::WithRectAndTitle({20, 20, 100, 100}, "quest");
+  auto window = Window::WithRectAndTitle({0, 0, WIN_W, WIN_H}, "Quest");
+  window->center();
+
+  const auto BOTTOM = window->contentHeight();
+  const auto GAP = 2_px;
+  auto y = GAP;
+
+  // Quest name
+  auto name = new Label({GAP, y, WIN_W, Element::TEXT_HEIGHT}, quest->name());
+  name->setColor(Color::HELP_TEXT_HEADING);
+  window->addChild(name);
+
+  quest->_window = window;
   Client::instance().addWindow(quest->_window);
   quest->_window->show();
 }
