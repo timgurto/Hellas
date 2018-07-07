bcf248753e8837740f77c03fb43b8bc74c39fa80
Tim Gurto <timgurto@gmail.com>
2018-07-08 06:05:31 +1000

    Editor: add missing file to last commit

--- tools/Map editor/Map editor/main.h
+++ tools/Map editor/Map editor/main.h
@@ -1,15 +1,16 @@
 #pragma once
 
+#include <set>
+#include <string>
+
 void initialiseSDL();
 void finaliseSDL();
 
+using FilesList = std::set<std::string>;
+FilesList findDataFiles(const std::string &path);
+
 void handleInput(unsigned timeElapsed);
 void render();
-
-template <typename T>
-T min(const T &lhs, const T &rhs) {
-  return lhs < rhs ? lhs : rhs;
-}
 
 enum Direction { UP, DOWN, LEFT, RIGHT };
 void pan(Direction dir);
