11316c9de941e75ac2d66022f0eab2f06d1abcf1
Tim Gurto <timgurto@gmail.com>
2018-07-11 03:09:09 +1000

    Editor: draw static NPCs

--- tools/Map editor/Map editor/EntityType.cpp
+++ tools/Map editor/Map editor/EntityType.cpp
@@ -23,4 +23,23 @@ void EntityType::load(Container& container, const std::string& filename) {
     xr.findAttr(elem, "yDrawOffset", et.drawRect.y);
     container[id] = et;
   }
+
+  for (auto elem : xr.getChildren("npcType")) {
+    auto et = EntityType{};
+
+    auto id = std::string{};
+    xr.findAttr(elem, "id", id);
+
+    auto imageFile = id;
+    xr.findAttr(elem, "imageFile", imageFile);
+    et.image = {"../../Images/NPCs/" + imageFile + ".png", Color::MAGENTA};
+    et.drawRect.w = et.image.width();
+    et.drawRect.h = et.image.height();
+
+    xr.findRectChild("collisionRect", elem, et.collisionRect);
+
+    xr.findAttr(elem, "xDrawOffset", et.drawRect.x);
+    xr.findAttr(elem, "yDrawOffset", et.drawRect.y);
+    container[id] = et;
+  }
 }
