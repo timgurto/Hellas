576e4dbdd52c8fe1ccfb422c152d9b81f4d4a561
Tim Gurto <timgurto@gmail.com>
2018-08-16 18:00:55 +1000

    Add stub test for AI pathfinding

---
+++ src/testing/test-ai.cpp
@@ -0,0 +1,11 @@
+#include "TestClient.h"
+#include "TestServer.h"
+#include "testing.h"
+
+TEST_CASE("NPCs can get around obstacles") {
+  GIVEN("A wall between an NPC and a player") {
+    WHEN("The NPC becomes aware of the user") {
+      THEN("It can reach him") {}
+    }
+  }
+}
