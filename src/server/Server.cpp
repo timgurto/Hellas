489d38881b2c7fd2fc40070dff670fa87b6dafb6
Tim Gurto <timgurto@gmail.com>
2018-08-22 01:41:02 +1000

    Give each player a Sprint spell

--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -364,6 +364,7 @@ void Server::addUser(const Socket &socket, const std::string &name,
   // Send him his known spells
   auto knownSpellsString = userClass.generateKnownSpellsString();
   sendMessage(newUser.socket(), SV_KNOWN_SPELLS, knownSpellsString);
+  newUser.getClass().teachSpell("sprint");
 
   // Add user to location-indexed trees
   getCollisionChunk(newUser.location()).addEntity(&newUser);
