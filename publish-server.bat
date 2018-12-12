4d39d662b770409aac075d5bf6344a5c6d38a0c9
Tim Gurto <timgurto@gmail.com>
2018-12-13 06:57:51 +1100

    Update publish-server script to reflect new directory

--- publish-server.bat
+++ publish-server.bat
@@ -1,13 +1,13 @@
 @echo off
 
 rem Copy files
-copy server.exe "C:\Hellas server\"
-copy SDL2.dll "C:\Hellas server\"
-xcopy Data "C:\Hellas server\Data\" /E /Y
-xcopy logging "C:\Hellas server\logging\" /E /Y
-xcopy Images\Items "C:\Hellas server\Images\Items\" /E /Y
+copy server.exe "F:\Permanent\Hellas production server\"
+copy SDL2.dll "F:\Permanent\Hellas production server\"
+xcopy Data "F:\Permanent\Hellas production server\Data\" /E /Y
+xcopy logging "F:\Permanent\Hellas production server\logging\" /E /Y
+xcopy Images\Items "F:\Permanent\Hellas production server\Images\Items\" /E /Y
 
 rem Start server
 C:
-cd "C:\Hellas server"
+cd "F:\Permanent\Hellas production server"
 launch.bat
