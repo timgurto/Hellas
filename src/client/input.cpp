9fc2056241f0a5d9b403badd0eeeb5226ff876b8
Tim Gurto <timgurto@gmail.com>
2019-05-26 00:41:31 +1000

    Give higher priority to non-flat entities for mouse over

--- src/client/input.cpp
+++ src/client/input.cpp
@@ -542,14 +542,31 @@ Sprite *Client::getEntityAtMouse() {
       bottomEntity(nullptr, {0, mouseOffset.y + LOOKUP_MARGIN});
   auto lowerBound = _entities.lower_bound(&topEntity),
        upperBound = _entities.upper_bound(&bottomEntity);
+
+  // First pass: non-flat entities
   for (auto it = lowerBound; it != upperBound; ++it) {
     const auto &entity = **it;
+    if (entity.type()->isFlat()) continue;
     if (&entity == &_character) continue;  // Can't interact with self
     if (entity.type()->isDecoration()) continue;
     if (!entity.collision(mouseOffset)) continue;  // Crude collision check
     if (!entity.mouseIsOverRealPixel(mouseOffset)) continue;  // Detailed check
     mouseOverIt = it;
   }
+
+  // Second pass: flat entities
+  if (mouseOverIt == _entities.end())
+    for (auto it = lowerBound; it != upperBound; ++it) {
+      const auto &entity = **it;
+      if (!entity.type()->isFlat()) continue;
+      if (&entity == &_character) continue;  // Can't interact with self
+      if (entity.type()->isDecoration()) continue;
+      if (!entity.collision(mouseOffset)) continue;  // Crude collision check
+      if (!entity.mouseIsOverRealPixel(mouseOffset))
+        continue;  // Detailed check
+      mouseOverIt = it;
+    }
+
   if (mouseOverIt != _entities.end())
     return *mouseOverIt;
   else
