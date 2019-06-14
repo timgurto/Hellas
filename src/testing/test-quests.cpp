83e1886040f904be41a5a6990ad60e2cf5e8f033
Tim Gurto <timgurto@gmail.com>
2019-06-15 05:12:10 +1000

    Abandon all quests on spellcast

--- src/testing/test-quests.cpp
+++ src/testing/test-quests.cpp
@@ -1939,7 +1939,7 @@ TEST_CASE("Quest objective: cast a spell") {
     auto data = R"(
       <objectType id="questgiver" />
       <spell id="fireball" range=30 cooldown=2 >
-        <targets enemy=1 />
+        <targets self=1 />
         <function name="doDirectDamage" />
       </spell>
       <quest id="castAFireball" startsAt="questgiver" endsAt="questgiver">
@@ -1954,6 +1954,7 @@ TEST_CASE("Quest objective: cast a spell") {
     auto c = TestClient::WithDataString(data);
     s.waitForUsers(1);
     auto &user = s.getFirstUser();
+    user.getClass().teachSpell("fireball");
 
     WHEN("a user accepts it") {
       c.sendMessage(CL_ACCEPT_QUEST,
@@ -1967,6 +1968,20 @@ TEST_CASE("Quest objective: cast a spell") {
 
         THEN("he is still on a quest") {
           CHECK(user.questsInProgress().size() == 1);
+        }
+      }
+
+      AND_WHEN("he casts the spell") {
+        c.sendMessage(CL_CAST, "fireball");
+
+        AND_WHEN("he tries to complete it") {
+          c.sendMessage(CL_COMPLETE_QUEST,
+                        makeArgs("castAFireball", questgiver.serial()));
+          REPEAT_FOR_MS(100);
+
+          THEN("he is not on a quest") {
+            WAIT_UNTIL(user.questsInProgress().size() == 0);
+          }
         }
       }
     }
