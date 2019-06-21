0eda958860af05101ac71ed118038ccc172a76a9
Tim Gurto <timgurto@gmail.com>
2019-06-22 02:04:16 +1000

    Kill log: increase transparency with level difference

--- logging/kills.R
+++ logging/kills.R
@@ -38,7 +38,11 @@ for (i in 1:length(data$npcID)){
     
     pch = 1
     if (data$killerLevel[i] != data$npcLevel[i]){
-        color = add.alpha(color, 0.2)
+        levelDiff = abs(data$killerLevel[i] - data$npcLevel[i])
+        alpha = 1-(levelDiff*0.3)
+        alpha = max(alpha, 0.1)
+        color = add.alpha(color, alpha)
+        
         if (data$killerLevel[i] > data$npcLevel[i])
             pch = '+'
         else
@@ -52,11 +56,16 @@ for (i in 1:length(data$npcID)){
         pch=pch
     )
 }
+PLUS_GLYPH = 43
+MINUS_GLYPH = 45
+CIRCLE = 1
 legend(
     "bottomright",
-    #legend=(paste(classes, "killer")),
-    legend=classes,
-    fill=classColors
+    legend=c(classes, "Higher-level player", "Lower-level player"),
+    pch=c(CIRCLE, CIRCLE, CIRCLE, PLUS_GLYPH, MINUS_GLYPH),
+    col=c(classColors, "#bbbbbb", "#bbbbbb")
+    #fill=c(classColors, NULL,NULL),
+    #border=c("#bbbbbb","#bbbbbb","#bbbbbb",NULL,NULL)
 )
 
 dev.off()
