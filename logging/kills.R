7e3055a7e40832e3ab9daf82c18ec654fe507dbd
Tim Gurto <timgurto@gmail.com>
2019-07-03 13:09:34 +1000

    Account for number of combatant players in kill log

--- logging/kills.R
+++ logging/kills.R
@@ -25,6 +25,8 @@ add.alpha <- function(col, alpha=1){
                        rgb(x[1], x[2], x[3], alpha=alpha))  
 }
 
+data$duration = data$duration / data$threatCount
+
 plot(
     x=NULL,
     xlim=c(min(data$npcLevel), max(data$npcLevel)),
