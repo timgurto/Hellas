1945bb295b84d26f31248fafdf0b790e42d6b5b3
Tim Gurto <timgurto@gmail.com>
2019-10-21 04:18:47 +1100

    Modernise deconstruct-damaged-object test

--- src/testing/test-objects.cpp
+++ src/testing/test-objects.cpp
@@ -47,23 +47,30 @@ TEST_CASE("Dead objects don't block movement") {
 }
 
 TEST_CASE("Damaged objects can't be deconstructed") {
-  // Given a server and client;
-  // And a 'brick' object type with 2 health;
-  TestServer s = TestServer::WithData("pickup_bricks");
-  TestClient c = TestClient::WithData("pickup_bricks");
+  GIVEN("a 'brick' object with 1 out of 2 health") {
+    auto data = R"(
+      <item id="brick" durability="2" />
+      <objectType id="brick" deconstructs="brick">
+        <durability item="brick" quantity="1"/>
+      </objectType>
+    )";
+    TestServer s = TestServer::WithDataString(data);
+    TestClient c = TestClient::WithDataString(data);
 
-  // And a brick object exists with only 1 health
-  s.addObject("brick", {10, 15});
-  Object &brick = s.getFirstObject();
-  brick.reduceHealth(1);
-  REQUIRE(brick.health() == 1);
+    s.addObject("brick", {10, 15});
+    Object &brick = s.getFirstObject();
+    brick.reduceHealth(1);
+    REQUIRE(brick.health() == 1);
 
-  // When the user tries to deconstruct the brick
-  c.sendMessage(CL_DECONSTRUCT, makeArgs(brick.serial()));
+    WHEN("the user tries to deconstruct the brick") {
+      c.sendMessage(CL_DECONSTRUCT, makeArgs(brick.serial()));
 
-  // Then the object still exists
-  REPEAT_FOR_MS(100);
-  CHECK_FALSE(s.entities().empty());
+      THEN("the object still exists") {
+        REPEAT_FOR_MS(100);
+        CHECK_FALSE(s.entities().empty());
+      }
+    }
+  }
 }
 
 TEST_CASE("Out-of-range objects are forgotten", "[.slow][culling][only]") {
