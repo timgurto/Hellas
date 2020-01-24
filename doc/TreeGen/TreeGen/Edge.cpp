ac199c7af54b419684962d8ff35acb4640e39b82
Tim Gurto <timgurto@gmail.com>
2020-01-25 06:00:06 +1100

    Clang Format

--- doc/TreeGen/TreeGen/Edge.cpp
+++ doc/TreeGen/TreeGen/Edge.cpp
@@ -1,22 +1,19 @@
 #include "Edge.h"
 
-Edge::Edge(const std::string &from, const std::string &to, EdgeType typeArg, double chanceArg):
-    parent(from),
-    child(to),
-    type(typeArg),
-    chance(chanceArg)
-    {}
+Edge::Edge(const std::string &from, const std::string &to, EdgeType typeArg,
+           double chanceArg)
+    : parent(from), child(to), type(typeArg), chance(chanceArg) {}
 
 bool Edge::operator==(const Edge &rhs) const {
-    return parent == rhs.parent && child == rhs.child;
-}
-   
-bool Edge::operator<(const Edge &rhs) const{
-    if (parent != rhs.parent) return parent < rhs.parent;
-    return child < rhs.child;
+  return parent == rhs.parent && child == rhs.child;
 }
 
-std::ostream &operator<<(std::ostream &lhs, const Edge &rhs){
-    lhs << rhs.parent << " -> " << rhs.child;
-    return lhs;
+bool Edge::operator<(const Edge &rhs) const {
+  if (parent != rhs.parent) return parent < rhs.parent;
+  return child < rhs.child;
+}
+
+std::ostream &operator<<(std::ostream &lhs, const Edge &rhs) {
+  lhs << rhs.parent << " -> " << rhs.child;
+  return lhs;
 }
--- doc/TreeGen/TreeGen/Edge.h
+++ doc/TreeGen/TreeGen/Edge.h
@@ -3,31 +3,32 @@
 
 #include <string>
 
-enum EdgeType{
-    GATHER,
-    CONSTRUCT_FROM_ITEM,
-    GATHER_REQ,
-    CONSTRUCTION_REQ,
-    LOOT,
-    INGREDIENT,
-    TRANSFORM,
+enum EdgeType {
+  GATHER,
+  CONSTRUCT_FROM_ITEM,
+  GATHER_REQ,
+  CONSTRUCTION_REQ,
+  LOOT,
+  INGREDIENT,
+  TRANSFORM,
 
-    UNLOCK_ON_GATHER,
-    UNLOCK_ON_ACQUIRE,
-    UNLOCK_ON_CRAFT,
-    UNLOCK_ON_CONSTRUCT,
+  UNLOCK_ON_GATHER,
+  UNLOCK_ON_ACQUIRE,
+  UNLOCK_ON_CRAFT,
+  UNLOCK_ON_CONSTRUCT,
 
-    DEFAULT
+  DEFAULT
 };
 
-struct Edge{
-    std::string parent, child;
-    EdgeType type;
-    double chance;
-    
-    Edge(const std::string &from, const std::string &to, EdgeType type, double chance = 1.0);
-    bool operator==(const Edge &rhs) const;
-    bool operator<(const Edge &rhs) const;
+struct Edge {
+  std::string parent, child;
+  EdgeType type;
+  double chance;
+
+  Edge(const std::string &from, const std::string &to, EdgeType type,
+       double chance = 1.0);
+  bool operator==(const Edge &rhs) const;
+  bool operator<(const Edge &rhs) const;
 };
 std::ostream &operator<<(std::ostream &lhs, const Edge &rhs);
 
