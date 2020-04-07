45d7a81277ae44d388a18c5bd25db48a49b96637
Tim Gurto <timgurto@gmail.com>
2020-04-08 08:49:35 +1000

    Apply trust in client movement to driven vehicles
    This solves a problem where vehicles were, in effect, much slower than clients thought.

--- src/server/Vehicle.cpp
+++ src/server/Vehicle.cpp
@@ -1,5 +1,11 @@
 #include "Vehicle.h"
+
 #include "VehicleType.h"
 
 Vehicle::Vehicle(const VehicleType *type, const MapPoint &loc)
     : Object(type, loc) {}
+
+bool Vehicle::shouldMoveWhereverRequested() const {
+  auto isDriving = !_driver.empty();
+  return isDriving;
+}
