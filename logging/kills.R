4c70886c524da557b43f9fae74eb68fb241cc626
Tim Gurto <timgurto@gmail.com>
2020-04-22 04:08:34 +1000

    Change the way kills.R plots combat times

--- logging/kills.R
+++ logging/kills.R
@@ -9,65 +9,46 @@ data <- read.csv(
     file="../kills.log",
     header=TRUE,
     sep=",",
-    colClasses=c("character","character","integer","integer","integer","integer","integer")
+    colClasses=c("character","integer","character","integer","integer","integer","integer")
 )
 data$duration = 1.0*data$duration/1000
 data$isRanged = data$isRanged != 0
 
-classes = sort(unique(data$killingClass))
-classColors = brewer.pal(length(classes), "Set1")
 
-add.alpha <- function(col, alpha=1){
-  if(missing(col))
-    stop("Please provide a vector of colours.")
-  apply(sapply(col, col2rgb)/255, 2, 
-                     function(x) 
-                       rgb(x[1], x[2], x[3], alpha=alpha))  
-}
 
 data$duration = data$duration * data$threatCount
 
 plot(
     x=NULL,
-    xlim=c(min(data$npcLevel), max(data$npcLevel)),
-    ylim=c(min(data$duration), max(data$duration)),
-    xlab="NPC level",
-    ylab="Fight duration (s)"
+    xlim=c(0.5, max(data$killerLevel)+0.5),
+    ylim=c(0.5, max(data$npcLevel)+0.5),
+    xlab="Player level",
+    ylab="Mob level"
 )
-for (i in 1:length(data$npcID)){
-    classIndex = match(data$killingClass[i], classes)
-    color = classColors[classIndex]
-    
-    pch = 1
-    if (data$killerLevel[i] != data$npcLevel[i]){
-        levelDiff = abs(data$killerLevel[i] - data$npcLevel[i])
-        alpha = 1-(levelDiff*0.3)
-        alpha = max(alpha, 0.1)
-        color = add.alpha(color, alpha)
-        
-        if (data$killerLevel[i] > data$npcLevel[i])
-            pch = '+'
-        else
-            pch = '-'
-    }
+for (playerLvl in min(data$killerLevel):max(data$killerLevel)){
+for (mobLvl in min(data$npcLevel):max(data$npcLevel)){
+    fightTimes = vector("numeric", 0)
 
-    points(
-        x=jitter(data$npcLevel[i]),
-        y=data$duration[i],
-        col=color,
-        pch=pch
+    for (i in 1:length(data$npcID)){
+        if (data$threatCount[i] != 1) {next}
+        if (data$isRanged[i]) {next}
+        
+        if (data$npcLevel[i] != mobLvl) {next}
+        if (data$killerLevel[i] != playerLvl) {next}
+        
+        fightTimes = c(fightTimes, data$duration[i])
+    }
+    
+    if (length(fightTimes) == 0) {next} # Don't draw anything if no data
+    
+    fightTime = mean(fightTimes)
+    text(
+        x=playerLvl,
+        y=mobLvl,
+        labels=fightTime
     )
-}
-PLUS_GLYPH = 43
-MINUS_GLYPH = 45
-CIRCLE = 1
-legend(
-    "topleft",
-    legend=c(classes, "Higher-level player", "Lower-level player"),
-    pch=c(CIRCLE, CIRCLE, CIRCLE, PLUS_GLYPH, MINUS_GLYPH),
-    col=c(classColors, "#bbbbbb", "#bbbbbb")
-    #fill=c(classColors, NULL,NULL),
-    #border=c("#bbbbbb","#bbbbbb","#bbbbbb",NULL,NULL)
-)
+}}
+
+
 
 dev.off()
