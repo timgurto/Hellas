eab184fccbeb9a855ac2394eb4d0e5b524b40f5a
Tim Gurto <timgurto@gmail.com>
2020-04-22 04:19:09 +1000

    Reformat kills.R to be more efficient

--- logging/kills.R
+++ logging/kills.R
@@ -13,10 +13,22 @@ data <- read.csv(
 )
 data$duration = 1.0*data$duration/1000
 data$isRanged = data$isRanged != 0
-
-
-
 data$duration = data$duration * data$threatCount
+
+# 1D vector storing 2D data:
+#
+#                             Player level
+#       1             2             3             4             5
+# [ . . . . . ] [ . . . . . ] [ . . . . . ] [ . . . . . ] [ . . . . . ]
+#   1 2 3 4 5     1 2 3 4 5     1 2 3 4 5     1 2 3 4 5     1 2 3 4 5
+#                               Mob level
+#
+MAX_PLAYER_LVL = max(data$killerLevel)
+MAX_MOB_LVL = max(data$npcLevel)
+VECTOR_SIZE = MAX_PLAYER_LVL * MAX_MOB_LVL
+combatTimesSum = vector("numeric", VECTOR_SIZE)
+combatCount = vector("numeric", VECTOR_SIZE)
+
 
 plot(
     x=NULL,
@@ -25,29 +37,35 @@ plot(
     xlab="Player level",
     ylab="Mob level"
 )
-for (playerLvl in min(data$killerLevel):max(data$killerLevel)){
-for (mobLvl in min(data$npcLevel):max(data$npcLevel)){
-    fightTimes = vector("numeric", 0)
 
-    for (i in 1:length(data$npcID)){
-        if (data$threatCount[i] != 1) {next}
-        if (data$isRanged[i]) {next}
-        
-        if (data$npcLevel[i] != mobLvl) {next}
-        if (data$killerLevel[i] != playerLvl) {next}
-        
-        fightTimes = c(fightTimes, data$duration[i])
-    }
+# Organise data into "2D" arrays
+for (i in 1:length(data$npcID)){
+    if (data$threatCount[i] != 1) {next}
+    if (data$isRanged[i]) {next}
     
-    if (length(fightTimes) == 0) {next} # Don't draw anything if no data
+    playerLvl = data$killerLevel[i]
+    mobLvl = data$npcLevel[i]
+    index = (playerLvl-1) * MAX_MOB_LVL + mobLvl
     
-    fightTime = mean(fightTimes)
+    combatTimesSum[index] = combatTimesSum[index] + data$duration[i]
+    combatCount[index] = combatCount[index] + 1
+}
+    
+for (playerLvl in 1:MAX_PLAYER_LVL){
+for (mobLvl in 1:MAX_MOB_LVL){
+    index = (playerLvl-1) * MAX_MOB_LVL + mobLvl
+    
+    if (combatCount[index] == 0) {next} # Don't draw anything if no data
+    
+    fightTime = combatTimesSum[index] / combatCount[index]
     text(
         x=playerLvl,
         y=mobLvl,
         labels=fightTime
     )
-}}
+    
+}
+}
 
 
 
