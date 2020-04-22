9937d69da160da7b97b0c3a3ab598ceb18876dd6
Tim Gurto <timgurto@gmail.com>
2020-04-23 01:29:53 +1000

    Split kills plot into three classes

--- logging/kills.R
+++ logging/kills.R
@@ -1,9 +1,16 @@
 library("RColorBrewer")
 #options(warn=2)
 
-svg("kills.svg", width=16, height=16)
+svg("kills.svg", width=20, height=7)
 
-par(col="#bbbbbb", col.axis="#bbbbbb", col.lab="#bbbbbb", fg="#bbbbbb", bg="black")
+par(
+    mfrow=c(1,3),
+    col="#bbbbbb",
+    col.axis="#bbbbbb",
+    col.lab="#bbbbbb",
+    col.main="#bbbbbb",
+    fg="#bbbbbb",
+    bg="black")
 
 data <- read.csv(
     file="../kills.log",
@@ -26,91 +33,136 @@ data$duration = data$duration * data$threatCount
 MAX_PLAYER_LVL = max(data$killerLevel)
 MAX_MOB_LVL = max(data$npcLevel)
 VECTOR_SIZE = MAX_PLAYER_LVL * MAX_MOB_LVL
-combatTimesSum = vector("numeric", VECTOR_SIZE)
-combatCount = vector("numeric", VECTOR_SIZE)
+ath_combatTimesSum = vector("numeric", VECTOR_SIZE)
+ath_combatCount = vector("numeric", VECTOR_SIZE)
+sch_combatTimesSum = vector("numeric", VECTOR_SIZE)
+sch_combatCount = vector("numeric", VECTOR_SIZE)
+zea_combatTimesSum = vector("numeric", VECTOR_SIZE)
+zea_combatCount = vector("numeric", VECTOR_SIZE)
 
 # Organise data into "2D" arrays
 for (i in 1:length(data$npcID)){
     if (data$threatCount[i] != 1) {next}
-    if (data$isRanged[i]) {next}
     
     playerLvl = data$killerLevel[i]
     mobLvl = data$npcLevel[i]
     index = (playerLvl-1) * MAX_MOB_LVL + mobLvl
     
-    combatTimesSum[index] = combatTimesSum[index] + data$duration[i]
-    combatCount[index] = combatCount[index] + 1
+    if (data$killingClass[i] == "Athlete"){
+        ath_combatTimesSum[index] = ath_combatTimesSum[index] + data$duration[i]
+        ath_combatCount[index] = ath_combatCount[index] + 1
+    } else if (data$killingClass[i] == "Scholar"){
+        sch_combatTimesSum[index] = sch_combatTimesSum[index] + data$duration[i]
+        sch_combatCount[index] = sch_combatCount[index] + 1
+    } else if (data$killingClass[i] == "Zealot"){
+        zea_combatTimesSum[index] = zea_combatTimesSum[index] + data$duration[i]
+        zea_combatCount[index] = zea_combatCount[index] + 1
+    }
 }
 
 AXIS_MAX = max(MAX_PLAYER_LVL, MAX_MOB_LVL)
-plot(
-    x=NULL,
-    xlim=c(0.5, AXIS_MAX+0.5),
-    ylim=c(0.5, AXIS_MAX+0.5),
-    xlab="Player level",
-    ylab="Mob level"
-)
 
-coloursFromPalette = brewer.pal(11, "Spectral")
+palette = "Spectral" #"RdYlGn"
+coloursFromPalette = brewer.pal(11, palette)
 colours = colorRamp(coloursFromPalette)
 
+# Calculate min and max fight times, so that colours can be placed on a spectrum
 minFightTime = NA
 maxFightTime = NA    
 for (playerLvl in 1:MAX_PLAYER_LVL){
 for (mobLvl in 1:MAX_MOB_LVL){
     index = (playerLvl-1) * MAX_MOB_LVL + mobLvl
-    fightTime = combatTimesSum[index] / combatCount[index]
     
+    fightTime = ath_combatTimesSum[index] / ath_combatCount[index]
     if (is.na(minFightTime))
         minFightTime = fightTime
     else if (!is.na(fightTime) && fightTime < minFightTime)
         minFightTime = fightTime
-        
+    if (is.na(maxFightTime))
+        maxFightTime = fightTime
+    else if (!is.na(fightTime) && fightTime > maxFightTime)
+        maxFightTime = fightTime
+    
+    fightTime = sch_combatTimesSum[index] / sch_combatCount[index]
+    if (is.na(minFightTime))
+        minFightTime = fightTime
+    else if (!is.na(fightTime) && fightTime < minFightTime)
+        minFightTime = fightTime
+    if (is.na(maxFightTime))
+        maxFightTime = fightTime
+    else if (!is.na(fightTime) && fightTime > maxFightTime)
+        maxFightTime = fightTime
+    
+    fightTime = zea_combatTimesSum[index] / zea_combatCount[index]
+    if (is.na(minFightTime))
+        minFightTime = fightTime
+    else if (!is.na(fightTime) && fightTime < minFightTime)
+        minFightTime = fightTime
     if (is.na(maxFightTime))
         maxFightTime = fightTime
     else if (!is.na(fightTime) && fightTime > maxFightTime)
         maxFightTime = fightTime
 }}
 
-for (playerLvl in 1:MAX_PLAYER_LVL){
-for (mobLvl in 1:MAX_MOB_LVL){
-    index = (playerLvl-1) * MAX_MOB_LVL + mobLvl
-    
-    if (combatCount[index] == 0) {next} # Don't draw anything if no data
-    
-    fightTime = combatTimesSum[index] / combatCount[index]
-    
-    fightTimeNorm = (fightTime - minFightTime) / (maxFightTime - minFightTime)
-    fightTimeNorm = 1 - fightTimeNorm
-    colour = colours(fightTimeNorm)
-    r = colour[1][1]
-    g = colour[2][1]
-    b = colour[3][1]
-    colour = rgb(r, g, b, maxColorValue=255)
-    
-    brightness = (r+g+b)/3
-    if (brightness > 127)
-        textColor = "black"
-    else
-        textColor = "white"
-    
-    polygon(
-        x=c(playerLvl-0.5, playerLvl+0.5, playerLvl+0.5, playerLvl-0.5),
-        y=c(mobLvl+0.5, mobLvl+0.5, mobLvl-0.5, mobLvl-0.5),
-        col=colour,
-        border="black",
-        lwd=5
+for (class in c("Athlete", "Scholar", "Zealot"))
+{
+    plot(
+        x=NULL,
+        xlim=c(0.5, AXIS_MAX+0.5),
+        ylim=c(0.5, AXIS_MAX+0.5),
+        xlab="Player level",
+        ylab="Mob level",
+        asp=1,
+        bty="n",
+        main=class
     )
-    label = paste(round(fightTime, 1), "s", sep="")
-    text(
-        x=playerLvl,
-        y=mobLvl,
-        labels=label,
-        col=textColor
-    )
-    
-}}
 
+    for (playerLvl in 1:MAX_PLAYER_LVL){
+    for (mobLvl in 1:MAX_MOB_LVL){
+        index = (playerLvl-1) * MAX_MOB_LVL + mobLvl
+        
+        if (class == "Athlete"){
+            if (ath_combatCount[index] == 0) {next} # Don't draw anything if no data
+            fightTime = ath_combatTimesSum[index] / ath_combatCount[index]
+        } else if (class == "Scholar"){
+            if (sch_combatCount[index] == 0) {next} # Don't draw anything if no data
+            fightTime = sch_combatTimesSum[index] / sch_combatCount[index]
+        } else if (class == "Zealot"){
+            if (zea_combatCount[index] == 0) {next} # Don't draw anything if no data
+            fightTime = zea_combatTimesSum[index] / zea_combatCount[index]
+        }
+        
+        fightTimeNorm = (fightTime - minFightTime) / (maxFightTime - minFightTime)
+        fightTimeNorm = 1 - fightTimeNorm
+        colour = colours(fightTimeNorm)
+        r = colour[1][1]
+        g = colour[2][1]
+        b = colour[3][1]
+        colour = rgb(r, g, b, maxColorValue=255)
+        
+        brightness = (r+g+b)/3
+        if (brightness > 127)
+            textColor = "black"
+        else
+            textColor = "white"
+        
+        polygon(
+            x=c(playerLvl-0.5, playerLvl+0.5, playerLvl+0.5, playerLvl-0.5),
+            y=c(mobLvl+0.5, mobLvl+0.5, mobLvl-0.5, mobLvl-0.5),
+            col=colour,
+            border="black"
+        )
+        label = round(fightTime, 0)
+        
+        text(
+            x=playerLvl,
+            y=mobLvl,
+            labels=label,
+            col=textColor
+        )
+        
+    }}
+}
 
 
 dev.off()
