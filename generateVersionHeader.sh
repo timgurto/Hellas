e9035f7f63e9a2192ee14d22995a0350d8c092ee
Tim Gurto <timgurto@gmail.com>
2020-04-28 02:56:02 +1000

    Upload current version when new client is shipped

--- generateVersionHeader.sh
+++ generateVersionHeader.sh
@@ -3,6 +3,8 @@
 revisioncount=`git log --oneline | wc -l`
 projectversion=`git describe --tags --long`
 
+echo "$projectversion" > version.txt
+
 echo "#define VERSION \"$projectversion\""
 echo "#define VERSION \"$projectversion\"" > src/version.h
 git add src/version.h
