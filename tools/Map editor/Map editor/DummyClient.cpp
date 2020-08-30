9eb635d6592714877090ae11698cba8ab7a1a2ed
Tim Gurto <timgurto@gmail.com>
2020-08-31 03:08:37 +1000

    Define Client::images in map editor

--- tools/Map editor/Map editor/DummyClient.cpp
+++ tools/Map editor/Map editor/DummyClient.cpp
@@ -4,6 +4,7 @@
 const int Client::ICON_SIZE = 0;
 const int Client::SCREEN_X = 0;
 const int Client::SCREEN_Y = 0;
-Client *Client::_instance = nullptr;
 
 bool isDebug() { return true; }
+
+Client::CommonImages Client::images;
