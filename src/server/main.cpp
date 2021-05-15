bc1d5f34ed31bcac1d96d01de221150da2d5d7dc
Tim Gurto <timgurto@gmail.com>
2021-05-15 22:24:01 +1000

    Skip Windows crash dialog, so that server-restart script always works

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,4 +1,5 @@
 #include <SDL.h>
+
 #include <cstdlib>
 #include <ctime>
 #include <string>
@@ -12,7 +13,13 @@ FILE __iob_func[3] = {*stdin, *stdout, *stderr};
 
 Args cmdLineArgs;
 
+static void disableWindowsCrashDialog() {
+  SetErrorMode(SEM_FAILCRITICALERRORS | SEM_NOGPFAULTERRORBOX);
+}
+
 int main(int argc, char* argv[]) {
+  disableWindowsCrashDialog();
+
   cmdLineArgs.init(argc, argv);
 
   srand(static_cast<unsigned>(time(0)));
