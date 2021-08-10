df670035d2324337009d3970b31c6aaf9645cd85
Tim Gurto <timgurto@gmail.com>
2021-08-11 05:16:37 +1000

    Rearrange suffix test

--- src/testing/test-suffixes.cpp
+++ src/testing/test-suffixes.cpp
@@ -4,12 +4,9 @@
 #include "testing.h"
 
 TEST_CASE_METHOD(ServerAndClientWithData, "Suffixes add stats", "[suffixes]") {
-  GIVEN("a sword with a +1-armour suffix") {
-    useData(R"(
-      <item id="sword" gearSlot="weapon" >
-        <randomSuffix fromSet="suffixes" />
-      </item>
-
+  GIVEN("a +1-armour suffix") {
+    auto data = ""s;
+    data = R"(
       <suffixSet id="suffixes" >
         <suffix id="extraArmour" />
       </suffixSet>
@@ -17,21 +14,30 @@ TEST_CASE_METHOD(ServerAndClientWithData, "Suffixes add stats", "[suffixes]") {
       <suffix id="extraArmour">
         <stats armour="1" />
       </suffix>
-    )");
+    )";
 
-    WHEN("the user gets a sword") {
-      const auto &sword = server->getFirstItem();
-      user->giveItem(&sword);
+    GIVEN("a sword with the suffix but no base stats") {
+      data += R"(
+        <item id="sword" gearSlot="weapon" >
+          <randomSuffix fromSet="suffixes" />
+        </item>
+      )";
+      useData(data.c_str());
 
-      AND_WHEN("he equips it") {
-        client->sendMessage(
-            CL_SWAP_ITEMS,
-            makeArgs(Serial::Inventory(), 0, Serial::Gear(), Item::WEAPON));
-        WAIT_UNTIL(user->gear()[Item::WEAPON].first.hasItem());
+      WHEN("the user gets a sword") {
+        const auto &sword = server->getFirstItem();
+        user->giveItem(&sword);
 
-        THEN("he has 1 armour") {
-          const auto expectedArmour = ArmourClass{1};
-          CHECK(user->stats().armor == expectedArmour);
+        AND_WHEN("he equips it") {
+          client->sendMessage(
+              CL_SWAP_ITEMS,
+              makeArgs(Serial::Inventory(), 0, Serial::Gear(), Item::WEAPON));
+          WAIT_UNTIL(user->gear()[Item::WEAPON].first.hasItem());
+
+          THEN("he has 1 armour") {
+            const auto expectedArmour = ArmourClass{1};
+            CHECK(user->stats().armor == expectedArmour);
+          }
         }
       }
     }
