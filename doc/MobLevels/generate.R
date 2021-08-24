0ce90d7bee842f171994efc545fefa42a1ac2470
Tim Gurto <timgurto@gmail.com>
2021-08-25 04:09:40 +1000

    Draw spawn points as white cirlces in MobLevels plot

--- doc/MobLevels/generate.R
+++ doc/MobLevels/generate.R
@@ -8,10 +8,15 @@ svg("plot.svg", width=12, height=12, bg=NA)
 map <- readPNG("..\\..\\Images\\map.png")
 maxCoord = 3000 * 32
 plot(NULL, xlim=c(0, maxCoord), ylim=c(maxCoord ,0))
-rasterImage(map, 0, 0, maxCoord, maxCoord)
+rasterImage(map, 0, maxCoord, maxCoord, 0)
 
 # For each entry
+for (i in 1:length(data$level)){
     # Circle radius based on quantity
-    # Circle colour based on level
-    # Draw circle at x,y
+    circleSize=1
     
+    # Circle colour based on level
+    
+    # Draw circle at x,y
+    points(data$x[i], data$y[i], pch=21, bg="white", cex=circleSize, col="transparent")
+}
