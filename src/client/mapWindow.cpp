33d0f0e456cf956a6118371670f016031182cdae
Tim Gurto <timgurto@gmail.com>
2021-10-01 04:11:49 +1000

    Uncomment Halloween-event map marker

--- src/client/mapWindow.cpp
+++ src/client/mapWindow.cpp
@@ -114,8 +114,7 @@ void Client::updateMapWindow(Element &e) {
 
   // TODO: STOP HARDCODING THIS
   // Halloween event:
-  // client.addIconToMap({33000, 33600}, &images.mapRespawn, "Thessaly
-  // Graveyard");
+  client.addIconToMap({33000, 33600}, &images.mapRespawn, "Thessaly Graveyard");
   //
   // Anzac Day event:
   // client.addIconToMap({64674, 17774}, &images.mapRespawn,
