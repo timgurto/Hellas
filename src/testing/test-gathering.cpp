2cb1338474f6ffecd411d4370f5f4f580d316fd9
Tim Gurto <timgurto@gmail.com>
2021-11-05 11:22:40 +1100

    Rewrite gathering-randomness test using fixture

--- src/testing/test-gathering.cpp
+++ src/testing/test-gathering.cpp
@@ -35,27 +35,31 @@ One gather worth of 1 million units of iron
 This is to test the new gather algorithm, which would favor rocks rather than
 iron. It will fail randomly about 1 every 1000 times
 */
-TEST_CASE("Gather chance is by gathers, not quantity", "[gathering]") {
-  auto s = TestServer::WithData("rare_iron");
-  auto c = TestClient::WithData("rare_iron");
+TEST_CASE_METHOD(ServerAndClientWithData,
+                 "Gather chance is by gathers, not quantity", "[gathering]") {
+  useData(R"(
+    <objectType id="ironDeposit" canGather=1 gatherTime=100>
+        <yield id="rock" initialMean=1000 initialSD=0 />
+        <yield id="iron" initialMean=1000000 initialSD=0 gatherMean=1000000 gatherSD=0 />
+    </objectType>
+    <item id="rock" />
+    <item id="iron" stackSize=1000000 />
+  )");
 
   // Add a single iron deposit
-  s.addObject("ironDeposit", {10, 10});
-  s.waitForUsers(1);
-  WAIT_UNTIL(c.objects().size() == 1);
+  const auto &ironDeposit = server->addObject("ironDeposit", {10, 10});
+  WAIT_UNTIL(client->objects().size() == 1);
 
   // Gather
-  auto serial = c.objects().begin()->first;
-  c.sendMessage(CL_GATHER, makeArgs(serial));
-  auto &user = s.getFirstUser();
-  WAIT_UNTIL(user.action() ==
+  client->sendMessage(CL_GATHER, makeArgs(ironDeposit.serial()));
+  WAIT_UNTIL(user->action() ==
              User::Action::GATHER);  // Wait for gathering to start
-  WAIT_UNTIL(user.action() ==
+  WAIT_UNTIL(user->action() ==
              User::Action::NO_ACTION);  // Wait for gathering to finish
 
   // Make sure user has a rock, and not the iron
-  const auto &item = *s.items().find(ServerItem("rock"));
-  CHECK(user.inventory(0).type() == &item);
+  const auto *rock = (*server)->findItem("rock");
+  CHECK(user->inventory(0).type() == rock);
 }
 
 TEST_CASE("Minimum yields", "[gathering]") {
