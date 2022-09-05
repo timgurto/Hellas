f7b0cc9e06101c5ae1d6ca9d0d588900686bd73b
Tim Gurto <timgurto@gmail.com>
2022-09-06 04:24:35 +1000

    Don't overwrite .csv files when publishing server

--- publish-server.bat
+++ publish-server.bat
@@ -5,7 +5,7 @@ copy server.exe "F:\Permanent\Hellas production server\"
 copy SDL2.dll "F:\Permanent\Hellas production server\"
 copy libcurl.dll "F:\Permanent\Hellas production server\"
 xcopy Data "F:\Permanent\Hellas production server\Data\" /E /Y
-xcopy logging "F:\Permanent\Hellas production server\logging\" /E /Y
+xcopy logging "F:\Permanent\Hellas production server\logging\" /E /Y /EXCLUDE:*.csv
 xcopy Images\Items "F:\Permanent\Hellas production server\Images\Items\" /E /Y
 
 rem Start server
